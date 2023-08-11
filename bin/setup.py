8cc14d861e7af947057020c9b52b0844b98a3724
taiyo2001 <taiyo2778@gmail.com>
2023-08-12 01:41:04 +0900

    add: setupファイルを作成(pythonのセットアップファイルはWIP)

---
+++ bin/setup.py
@@ -0,0 +1,2 @@
+# setup.shで実行
+# TODO:pythonファイルでもsetupを作成？？
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "== start setup =="
+
+# APP_PATHをPYTHONPATHに追加
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+echo "== end setup =="
