54e53a7392a9096c7b3661835309ffc93d9ece32
taiyo2001 <taiyo2778@gmail.com>
2023-12-17 03:05:16 +0900

    fix: shuffle

--- config/setting.py
+++ config/setting.py
@@ -4,6 +4,9 @@ import os
 # 定数の設定
 const.APP_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-# const.TRAIN_PATH = const.APP_PATH + '/data/img' # 45のみの使用
-const.TRAIN_PATH = const.APP_PATH + '/data/img_full' # 10, 15. 45のすべてを使用
+# const.TRAIN_DIR = '/img'       # 45のみの使用
+# const.TRAIN_DIR = '/img_full'  # 10, 15. 45のすべてを使用
+const.TRAIN_DIR = '/img_new'   # 新たに作成したものを使用
+
+const.TRAIN_PATH = const.APP_PATH + '/data' + const.TRAIN_DIR
 const.CHECKPOINT_PATH = const.APP_PATH + '/data/model'
--- module/image_loader.py
+++ module/image_loader.py
@@ -9,8 +9,7 @@ from albumentations.pytorch import ToTensorV2
 
 
 #画像データ拡張の関数
-def get_train_transform(image_height, image_width):
-
+def get_train_transform(image_height, image_width, horizontal_flip=0.25, vertical_flip=0.25):
     return alb.Compose(
         [
             #リサイズ(元画像ですでにしているが)
@@ -18,9 +17,9 @@ def get_train_transform(image_height, image_width):
             #正規化(こちらの細かい値はalbumentations.augmentations.transforms.Normalizeのデフォルトの値を適用)
             alb.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             #水平フリップ（pはフリップする確率）
-            alb.HorizontalFlip(p=0.25),
+            alb.HorizontalFlip(p=horizontal_flip),
             #垂直フリップ
-            alb.VerticalFlip(p=0.25),
+            alb.VerticalFlip(p=vertical_flip),
             ToTensorV2(),
         ])
 
@@ -28,7 +27,8 @@ def get_train_transform(image_height, image_width):
 class LoadDataSet(Dataset):
         def __init__(self, path, image_height, image_width, transform=None):
             self.path = path
-            self.folders = os.listdir(path)
+            folders = os.listdir(path)
+            self.folders = sorted(folders, key=lambda x: tuple(map(int, x.split('-'))))
             self.image_height = image_height
             self.image_width = image_width
             self.transforms = transform # get_train_transform(256, 256)
@@ -48,23 +48,14 @@ class LoadDataSet(Dataset):
             # print(f'mask_path: {mask_path}')
 
             #画像データの取得
-            # 3次元の処理を2次元に変更するのがいろいろめんどくさいから3次元の型で扱うことにする
-            ## exec img
             # img = io.imread(image_path)[:,:,:3].astype('float32')
             img = io.imread(image_path)
             # TODO: 正答率がでたら比較としてfloat32でも試してみる
-            # print(f'2d img.shape: {img.shape}') # 白黒なのでチャネル数が省略されて2次元データになっている
             img = self.conv_2D_to_3Darray(img)
-            # print(f'3d img.shape: {img.shape}')
 
-
-            ## exec mask
-            # img = transform.resize(img,(256,256))
-            ## mask = io.imread(mask_path)
             # maskの量が2つ以上になったらここの関数を実行してすべてを得る
             # mask = self.get_mask(mask_folder, 256, 256).astype('float32')
             mask = self.get_mask(mask_folder, self.image_height, self.image_width) # nint8でOK?
-            # print(f'calced mask.shape: {mask.shape}')
 
             # 前処理をするためにひとつにまとめる
             augmented = self.transforms(image=img, mask=mask)
@@ -73,22 +64,16 @@ class LoadDataSet(Dataset):
             return (img,mask)
 
         def conv_2D_to_3Darray(self, arr):
-            # print(f'exec conv_2D_to_3Darray')
             image = Image.fromarray(arr)
             image = image.convert("RGB")
             # 変換するならここでfloat32にする
             arr = np.asarray(image, np.uint8)
-            # print(f'to3d arr shape: {arr.shape}')
             return arr
 
         def conv_3D_to_2Darray(self, arr):
-            # print(f'exec conv_3D_to_2Darray')
             image = Image.fromarray(arr)
             image = image.convert("L")
             arr = np.asarray(image, np.uint8) # 変換するならここでfloat32にする
-            # print(f'to2d arr shape: {arr.shape}')
-            # arr = np.transpose(arr, (2, 0, 1))
-            # print(f'arr transpose shape: {arr.shape}')
             return arr
 
         #マスクデータの取得
@@ -96,11 +81,13 @@ class LoadDataSet(Dataset):
             mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=bool)
             # print(f'len(os.listdir(mask_folder)): {len(os.listdir(mask_folder))}')
             for mask_ in os.listdir(mask_folder):
-                    mask_ = io.imread(os.path.join(mask_folder,mask_))
-                    # RGBからLへ
-                    mask_ = self.conv_3D_to_2Darray(mask_)
-                    mask_ = transform.resize(mask_, (IMG_HEIGHT, IMG_WIDTH))
-                    mask_ = np.expand_dims(mask_,axis=-1)
-                    mask = np.maximum(mask, mask_)
+                mask_ = io.imread(os.path.join(mask_folder,mask_))
+                # RGBからLへ
+                mask_ = self.conv_3D_to_2Darray(mask_)
+                mask_ = transform.resize(mask_, (IMG_HEIGHT, IMG_WIDTH))
+                mask_ = np.expand_dims(mask_,axis=-1)
+                # mask = np.maximum(mask, mask_)
+                mask = mask_
+
 
             return mask
