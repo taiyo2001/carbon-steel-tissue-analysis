6a5c96d0f431d23b9c4edb1a016f0f020fbcc837
taiyo2001 <taiyo2778@gmail.com>
2023-12-19 21:38:48 +0900

    fix: bin file

--- bin/exec_ipynb.sh
+++ bin/exec_ipynb.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+# XXX: exec with sh command. If you don't, you may get an error
 FILE_NAME=${1}
 NUM_EPOCH=${2}
 
-# APP_PATH="$(dirname $(pwd))"
-APP_PATH=$(pwd)
-
-echo $APP_PATH
+CURRENT_DIR=$(dirname "$(readlink -f "$0")")
+APP_PATH=$(dirname "$CURRENT_DIR")
 
 echo "== start sh =="
 
 if [ -z $NUM_EPOCH ]; then
 
-  echo "exec 'jupyter nbconvert --to notebook --execute $APP_PATH/app/${FILE_NAME}.ipynb --output $APP_PATH/result/${FILE_NAME}.nbconvert.ipynb'"
+  echo "exec 'jupyter nbconvert --to notebook --execute $APP_PATH/app/$FILE_NAME.ipynb --output $APP_PATH/result/$FILE_NAME.nbconvert.ipynb'"
 
-  jupyter nbconvert --to notebook --execute $APP_PATH/app/${FILE_NAME}.ipynb --output $APP_PATH/result/${FILE_NAME}.nbconvert.ipynb
+  touch $APP_PATH/result/$FILE_NAME.nbconvert.ipynb
+
+  jupyter nbconvert --to notebook --execute $APP_PATH/app/$FILE_NAME.ipynb --output $APP_PATH/result/$FILE_NAME.nbconvert.ipynb
 
 else
 
-  echo "exec 'jupyter nbconvert --to notebook --execute $APP_PATH/app/${FILE_NAME}.ipynb --output $APP_PATH/result/${FILE_NAME}_epoch_${NUM_EPOCH}.nbconvert.ipynb'"
+  echo "exec 'jupyter nbconvert --to notebook --execute $APP_PATH/app/$FILE_NAME.ipynb --output $APP_PATH/result/${FILE_NAME}_epoch_${NUM_EPOCH}.nbconvert.ipynb'"
 
-  jupyter nbconvert --to notebook --execute $APP_PATH/app/${FILE_NAME}.ipynb --output $APP_PATH/result/${FILE_NAME}_epoch_${NUM_EPOCH}.nbconvert.ipynb
+  jupyter nbconvert --to notebook --execute $APP_PATH/app/$FILE_NAME.ipynb --output $APP_PATH/result/${FILE_NAME}_epoch_${NUM_EPOCH}.nbconvert.ipynb
 fi
 
 echo "== end sh =="
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
+# XXX: exec with source command. If you don't, you may get an error
 echo "== start setup =="
 
-# APP_PATHをPYTHONPATHに追加
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-# export PYTHONPATH=$PYTHONPATH:$(dirname $(pwd))
+CURRENT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+APP_PATH=$(dirname "$CURRENT_DIR")
+
+if [ -e "$APP_PATH/bin/exec_ipynb.sh" ]; then
+  chmod +x $APP_PATH/bin/exec_ipynb.sh
+  alias ipynb="sh $APP_PATH/bin/exec_ipynb.sh"
+  echo "exec sh exec_ipynb.sh at $APP_PATH/bin = ipynb [file_name] [epochs]"
+else
+  echo "Error: exec_ipynb.sh not found at $APP_PATH/bin"
+  exit 1
+fi
+
+# set module import source
+export PYTHONPATH=$PYTHONPATH:$APP_PATH
+
+chmod +x $APP_PATH/bin/exec_ipynb.sh
+alias ipynb="sh $APP_PATH/bin/exec_ipynb.sh"
+
+
+if [ ! -d "$APP_PATH/tmp" ]; then
+    mkdir "$APP_PATH/tmp"
+fi
+
+if [ ! -d "$APP_PATH/data" ]; then
+    mkdir "$APP_PATH/data"
+fi
+
+if [ ! -d "$APP_PATH/result/services" ]; then
+    mkdir "$APP_PATH/result/services"
+fi
 
 echo "== end setup =="
