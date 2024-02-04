4f1687cfa460aca3b9ce202edae091c8db09bfa8
taiyo2001 <taiyo2778@gmail.com>
2024-02-04 20:06:11 +0900

    fix: tissue analysis module

--- module/tissue_analysis.py
+++ module/tissue_analysis.py
@@ -1,9 +1,23 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+from typing import Union
 
 def display_input_label_pre_image(input, label, predicted, name):
+    """
+    画像解析結果を表示.
+
+    Parameters:
+    - input (numpy.ndarray): 解析対象の入力画像 (BGR カラー形式).
+    - label (numpy.ndarray): ラベル画像 (BGR カラー形式).
+    - predicted (numpy.ndarray): 予測画像 (BGR カラー形式).
+    - name (str): 画像に関する名前や識別子.
+
+    Returns:
+    None
+
+    画像解析の結果を入力画像、ラベル画像、予測画像の形で横に並べて表示.
+    """
     gray_label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
     gray_predicted = cv2.cvtColor(predicted, cv2.COLOR_BGR2GRAY)
 
@@ -23,11 +37,46 @@ def display_input_label_pre_image(input, label, predicted, name):
     plt.show()
 
 
-def exec_ferrite_analysis(ferrite_image):
+def exec_ferrite_analysis(ferrite_image: np.ndarray, expansion: Union[None, int] = None) -> None:
+    """
+    フェライトの画像解析を実行.
+
+    Parameters:
+    - ferrite_image (numpy.ndarray): 解析対象のフェライト画像. BGR カラー形式.
+    - expansion (None | int, optional): 輪郭拡張を行うかどうかを指定するフラグ. デフォルトは None. integerのときはkernelの大きさ.
+
+    Returns:
+    None
+
+    解析結果はプロットされ、コンソールに輪郭の情報が表示.
+    """
     gray_ferrite_image = cv2.cvtColor(ferrite_image, cv2.COLOR_BGR2GRAY)
 
-    # 二値化処理を行う
+    # 二値化処理
     _, binary_image = cv2.threshold(gray_ferrite_image, 128, 255, cv2.THRESH_BINARY)
+
+    if expansion is not None and isinstance(expansion, int):
+        print('exec expansion')
+        binary_image = cv2.bitwise_not(binary_image)
+
+        kernel = np.ones((expansion, expansion), np.uint8)
+        dilated_image = cv2.dilate(binary_image, kernel, iterations=1)
+        eroded_image = cv2.erode(dilated_image, kernel, iterations=1)
+
+        # print('binary_image')
+        # plt.figure(figsize=(10, 10))
+        # plt.imshow(binary_image)
+        # plt.show()
+
+        # print('dilated_image')
+        # plt.figure(figsize=(10, 10))
+        # plt.imshow(eroded_image)
+        # plt.show()
+
+        eroded_image = cv2.bitwise_not(eroded_image)
+        _, binary_image = cv2.threshold(eroded_image, 128, 255, cv2.THRESH_BINARY)
+
+
     # 輪郭を検出
     contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours, hierarchy = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -92,10 +141,21 @@ def exec_ferrite_analysis(ferrite_image):
     plt.show()
 
 
-def exec_perlite_analysis(perlite_image):
+def exec_perlite_analysis(perlite_image: np.ndarray) -> None:
+    """
+    パーライトの画像解析を実行.
+
+    Parameters:
+    - perlite_image (numpy.ndarray): 解析対象のパーライト画像. BGR カラー形式.
+
+    Returns:
+    None
+
+    解析結果はコンソールに輪郭の情報が表示され、プロットされた画像が表示.
+    """
     gray_perlite_image = cv2.cvtColor(perlite_image, cv2.COLOR_BGR2GRAY)
 
-    # 二値化処理を行う
+    # 二値化処理
     _, binary_image = cv2.threshold(gray_perlite_image, 128, 255, cv2.THRESH_BINARY)
 
     binary_image = cv2.bitwise_not(binary_image)
@@ -105,7 +165,7 @@ def exec_perlite_analysis(perlite_image):
     _, eroded_image = cv2.threshold(eroded_image, 200, 255, cv2.THRESH_BINARY)
 
     # 輪郭を検出
-    contours, _ = cv2.findContours(eroded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(eroded_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # 面積が閾値未満の輪郭を削除
     min_contour_area = 3
